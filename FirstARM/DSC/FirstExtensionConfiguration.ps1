24b63ada67d1df827cb343f9dfafea3804cea8d4
ascdev2016 <andre.schmidt@direkt-gruppe.de>
2016-07-05 10:21:34 +0200

    DSC FIle Added

---
+++ FirstARM/DSC/FirstExtensionConfiguration.ps1
@@ -0,0 +1,17 @@
+Configuration MyFile
+{
+
+Param ( [string] $nodeName )
+
+Import-DscResource -ModuleName PSDesiredStateConfiguration
+
+Node $nodeName
+  {
+    File MyFile {
+
+            DestinationPath = 'C:\Skripte\MyDSCFile.txt'
+            Ensure = 'Present'
+            Type = 'File'
+            Contents = 'DSC Rocks!'
+        }
+}
